9c7db337176a46872b0299ccc36203f7a82f8f35
Daniel <danieldawit33@gmail.com>
2022-05-30 14:13:30 -0400

    add org search

--- src/renderer/screens/SelectWinner.tsx
+++ src/renderer/screens/SelectWinner.tsx
@@ -1,54 +1,76 @@
 import React, { useCallback, useMemo, useState } from "react";
-import {
-  Box,
-  Button,
-  Grid,
-  IconButton,
-  ImageList,
-  ImageListItem,
-  ImageListItemBar,
-  ListSubheader
-} from "@mui/material";
+import { Button, Collapse, ImageList, ImageListItem, ImageListItemBar, TextField } from "@mui/material";
+import { TransitionGroup } from "react-transition-group";
 import { db } from "../../main/database";
 import { Organization } from "../../main/database/modules/organization/Organization";
 
 
 function SelectWinner() {
 
-  const [selected, setSelected] = useState<string[]>([]);
+  const [selected, setSelected] = useState<Set<string>>(new Set());
+  const [searched, setSearched] = useState("");
 
-  const addSelected = useCallback((item: string) => () =>  {
-    selected.push(item);
-    setSelected([...selected]);
-  }, [selected])
+  const toggleSelected = useCallback((item: string) => () => {
+    if (!selected.has(item)) {
+      selected.add(item);
+      setSelected(new Set(selected));
+    } else {
+      selected.delete(item);
+      setSelected(new Set(selected));
+    }
+  }, [selected]);
+
+  const handleSearchFieldChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const text = event.target.value;
+    setSearched(text);
+  }, [searched]);
+
+  const renderItem = useCallback((item: Organization) => {
+
+    const isSearchedFor = item.name.toLowerCase().includes(searched.toLowerCase());
+    if (!isSearchedFor) return null;
+
+    return (
+      <Collapse key={item.id} in={isSearchedFor} mountOnEnter unmountOnExit>
+        <ImageListItem key={item.image} sx={{}}>
+          <img
+            style={{
+              border: selected.has(item.id) ? "5px solid #98FF98" : undefined,
+              height: "400px"
+            }}
+            src={`${item.image}?fit=crop&auto=format`}
+            srcSet={`${item.image}?&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.name}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.name}
+            subtitle={item.name}
+            actionIcon={<Button
+              onClick={toggleSelected(item.id)}>{selected.has(item.id) ? "Remove" : "Select"}</Button>}
+          />
+        </ImageListItem>
+      </Collapse>
+    );
+  }, [selected, searched]);
 
   const organizations = useMemo(() => db.organizations.orgs, [db.organizations]);
 
   return (
-    <div style={{display: "flex", flex: 1, width: '100%', padding: 20}}>
+    <div style={{ display: "flex", flex: 1, width: "100%", padding: 20 }}>
 
-      <ImageList sx={{ width: '100%', height: '100%' }}>
+      <ImageList sx={{ width: "100%", height: "100%" }}>
         <ImageListItem key="Subheader" cols={2}>
-          <ListSubheader component="div">Participants</ListSubheader>
+          <TextField id={"org-search"} label={"Search"} variant={"filled"} sx={{
+            backgroundColor: "rgba(255,255,255,0.25)",
+            color: "white"
+          }} inputProps={{
+            style: {
+              color: "white"
+            }
+          }} onChange={handleSearchFieldChange} />
         </ImageListItem>
-        {[...organizations.values()].map((item: Organization) => (
-          <ImageListItem key={item.image} sx={{
-            borderColor: selected.includes(item.id) ? 'darkolivegreen' : undefined,
-            borderWidth: 3
-          }}>
-            <img
-              src={`${item.image}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.name}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              title={item.name}
-              subtitle={item.name}
-              actionIcon={<Button onClick={addSelected(item.id)}>Select</Button>}
-            />
-          </ImageListItem>
-        ))}
+        {[...organizations.values()].map(renderItem)}
       </ImageList>
 
     </div>
